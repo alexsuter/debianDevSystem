777a8ec298e900f11d2e859c94203f6a91fa7738
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2019-08-22 09:49:26 +0200

    refact: authorized curl posts as funct
    - consolidate POST request against remote jenkins in one function since they are heavy and prone to errors:
     * possibly with a user auth-challenge
     * possibly with a re-usable XSS prevention CRUM request
    - eases introduction on new feature such as verbose/logging mode

--- bin/jenkinsRun.sh
+++ bin/jenkinsRun.sh
@@ -53,18 +53,6 @@ function triggerBuilds() {
     BRANCH=$1
     echo -e "triggering builds for ${GREEN}${BRANCH}${NC}"
 
-    if [ -z ${JENKINS_TOKEN+x} ]
-    then
-        echo "Jenkins API token not found as enviroment variable called 'JENKINS_TOKEN'. Therefore password for jenkins must be entered:"
-        echo -n "Enter JENKINS password for $JENKINS_USER:" 
-        echo -n ""
-        read -s JENKINS_TOKEN
-        echo ""
-    fi
-
-    # get XSS preventention token
-    CRUMB=`wget -q --auth-no-challenge --user $JENKINS_USER --password $JENKINS_TOKEN --output-document - 'http://zugprojenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
-
     JOBS=$( getAvailableTestJobs )
     select RUN in none 'ivy-core_ci' 'ivy-core_product' $JOBS 'new view'
     do
@@ -80,12 +68,15 @@ function triggerBuilds() {
         RUN_JOB=${RUN}
         BRANCH_ENCODED=`encode $BRANCH`
         BUILD_URL="http://$JENKINS/job/$RUN_JOB/job/$BRANCH_ENCODED/build?delay=0sec"
-        RESPONSE=`curl --write-out %{http_code} --silent --output /dev/null -I -X POST -u "$JENKINS_USER:$JENKINS_TOKEN" "$BUILD_URL" -H "$CRUMB"`
+        RESPONSE=`jPost --write-out %{http_code} --silent --output /dev/null -I "$BUILD_URL"`
         echo "jenkins returned HTTP code : $RESPONSE"
         
         if [ "$RESPONSE" == 404 ] ; then
             # job may requires a manual rescan to expose our new branch
             rescanBranches "http://$JENKINS/job/$RUN_JOB/"
+            # re-try after re-scan:
+            RESPONSE=`jPost --write-out %{http_code} --silent --output /dev/null -I "$BUILD_URL"`
+            echo "jenkins returned HTTP code : $RESPONSE"
         fi
     done
 }
@@ -95,7 +86,7 @@ function rescanBranches()
   JOB_URL=$1
   ACTION="build?delay=0"
   SCAN_URL="$JOB_URL$ACTION"
-  HTTP_STATUS=`curl --write-out %{http_code} --silent --output /dev/null -I -L -X POST -u "$JENKINS_USER:$JENKINS_TOKEN" "$SCAN_URL"`
+  HTTP_STATUS=`jPost --write-out %{http_code} --silent --output /dev/null -I -L "$SCAN_URL"`
   echo "triggered rescan triggered for $SCAN_URL"
   
   if [[ $HTTP_STATUS == *"200"* ]]; then
@@ -116,14 +107,36 @@ function createView()
   BRANCH=$1
   BRANCH_ENCODED=`encode $BRANCH`
   MYVIEWS_URL="https://$JENKINS/user/${JENKINS_USER}/my-views"
-  curl -k -X POST -u "$JENKINS_USER:$JENKINS_TOKEN" -H "$CRUMB" --form name=test --form   mode=hudson.model.ListView --form json="{'name': '${BRANCH}', 'mode': 'hudson.model.ListView', 'useincluderegex': 'on'}" "${MYVIEWS_URL}/createView"
+  jPost --form name=test --form mode=hudson.model.ListView --form json="{'name': '${BRANCH}', 'mode': 'hudson.model.ListView', 'useincluderegex': 'on'}" "${MYVIEWS_URL}/createView"
   CONFIG_URL="${MYVIEWS_URL}/view/${BRANCH_ENCODED}/config.xml"
   curl -k -s -X GET "${CONFIG_URL}" -o viewConf.xml
   ISSUE_REGEX=$( echo $BRANCH | sed -e 's|.*/|\.*|')
   sed -e "s|<recurse>false</recurse>|<includeRegex>${ISSUE_REGEX}</includeRegex><recurse>true></recurse>|" viewConf.xml > viewConf2.xml
-  curl -k -X POST "${CONFIG_URL}" -u "$JENKINS_USER:$JENKINS_TOKEN" -H "$CRUMB" -H "Content-Type:text/xml" --data-binary "@viewConf2.xml"
+  jPost -H "Content-Type:text/xml" --data-binary "@viewConf2.xml"
   rm viewConf*.xml
   echo "View created: ${MYVIEWS_URL}/view/${BRANCH_ENCODED}/"
+}
+
+function jPost()
+{
+  if [ -z ${JENKINS_TOKEN+x} ]
+  then
+    echo "Jenkins API token not found as enviroment variable called 'JENKINS_TOKEN'. Therefore password for jenkins must be entered:"
+    echo -n "Enter JENKINS password for $JENKINS_USER:" 
+    echo -n ""
+    read -s JENKINS_TOKEN
+    export JENKINS_TOKEN=${JENKINS_TOKEN}
+    echo ""
+  fi
+  
+  # get XSS preventention token
+  if [ -z ${CRUMB+x} ]
+  then
+    CRUMB=`wget -q --auth-no-challenge --user $JENKINS_USER --password $JENKINS_TOKEN --output-document - 'http://zugprojenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
+    export CRUMB=${CRUMB}
+  fi
+  
+  curl -k POST -u "$JENKINS_USER:$JENKINS_TOKEN" -H "$CRUMB" "$@"
 }
 
 function encode()
